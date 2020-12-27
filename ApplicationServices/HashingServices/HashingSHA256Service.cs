48c6d01c4ddae7c105765700b993bbd9e3ddfcde
Erkeri <dararkari@gmail.com>
2020-12-27 12:44:33 +0300

    Add Hashing Service

---
+++ ApplicationServices/HashingServices/HashingSHA256Service.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ApplicationServices.HashingServices
+{
+    public class HashingSHA256Service : IHashingService
+    {
+        public string GetHash(string text)
+        {
+            var sha256Managed = new SHA256Managed();
+            byte[] hash = sha256Managed.ComputeHash(new UTF8Encoding().GetBytes("заебумба" + text));
+            string hashString = string.Empty;
+            foreach (var i in hash)
+                hashString += string.Format("{0:x2}", i);
+            return hashString;
+        }
+    }
+}
---
+++ ApplicationServices/HashingServices/IHashingService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationServices.HashingServices
+{
+    public interface IHashingService
+    {
+        string GetHash(string text);
+    }
+}

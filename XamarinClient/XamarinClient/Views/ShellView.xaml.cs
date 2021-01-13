8b0aaeb3f99297811e6248fe26e06d3a01d9fd29
asceso <asceso111@gmail.com>
2021-01-13 11:46:44 +0300

    small fix

--- XamarinClient/XamarinClient/Views/ShellView.xaml.cs
+++ XamarinClient/XamarinClient/Views/ShellView.xaml.cs
@@ -5,7 +5,7 @@ using Xamarin.Forms.Xaml;
 namespace XamarinClient.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ShellView : ContentView
+    public partial class ShellView : ContentPage
     {
         public ShellView() => InitializeComponent();
     }

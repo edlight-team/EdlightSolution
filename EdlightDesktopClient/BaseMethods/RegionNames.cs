9d8297201b7507833b979849eb37528d669720fb
Uchidero Krauch <37419270+asceso@users.noreply.github.com>
2021-05-13 23:51:12 +0300

    Создан каркас

---
+++ EdlightDesktopClient/BaseMethods/RegionNames.cs
@@ -0,0 +1,22 @@
+﻿namespace EdlightDesktopClient.BaseMethods
+{
+    internal static class RegionNames
+    {
+        /// <summary>
+        /// Расписание
+        /// </summary>
+        internal static string ScheduleRegion = nameof(ScheduleRegion);
+        /// <summary>
+        /// Профиль
+        /// </summary>
+        internal static string ProfileRegion = nameof(ProfileRegion);
+        /// <summary>
+        /// Обучение
+        /// </summary>
+        internal static string LearnRegion = nameof(LearnRegion);
+        /// <summary>
+        /// Настройки
+        /// </summary>
+        internal static string SettingsRegion = nameof(SettingsRegion);
+    }
+}
---
+++ EdlightDesktopClient/ViewModels/Settings/SettingsMainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Prism.Mvvm;
+
+namespace EdlightDesktopClient.ViewModels.Settings
+{
+    public class SettingsMainViewModel : BindableBase
+    {
+    }
+}

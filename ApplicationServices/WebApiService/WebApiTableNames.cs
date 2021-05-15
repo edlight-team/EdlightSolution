40dff57bd0817ebf5955f6d60055385d82742352
Erkeri <dararkari@gmail.com>
2021-05-15 18:21:31 +0300

    AddFillTest

--- ApplicationServices/WebApiService/WebApiTableNames.cs
+++ ApplicationServices/WebApiService/WebApiTableNames.cs
@@ -17,5 +17,8 @@
         public static string Users = nameof(Users);
         public static string UsersDialogs = nameof(UsersDialogs);
         public static string UsersRoles = nameof(UsersRoles);
+        public static string TestHeaders = nameof(TestHeaders);
+        public static string TestResults = nameof(TestResults);
+        public static string Tests = nameof(Tests);
     }
 }

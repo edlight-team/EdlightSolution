7863e58fb1fcb896f0e2ad695a8545ad50657c56
Uchidero Krauch <37419270+asceso@users.noreply.github.com>
2021-05-17 11:11:14 +0300

    Генератор имен

---
+++ NameGeneratorTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NameGeneratorTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            PersonGenerator.GeneratorSettings settings = new()
+            {
+                Age = true,
+                Language = PersonGenerator.Languages.English,
+                FirstName = true,
+                MiddleName = true,
+                LastName = true,
+                MinAge = 16,
+                MaxAge = 25
+            };
+            PersonGenerator.PersonGenerator generator = new(settings);
+
+            var generated = generator.Generate(150);
+
+            foreach (var item in generated)
+            {
+                Console.WriteLine($"{item.FirstName} {item.MiddleName} {item.LastName} {item.Age}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
